5a3ddc7e8399a0954bb5596bd122e30daf445d10
Nathan Archer <pheianox@tutanota.com>
2021-05-10 13:19:29 +0400

    Fix print issues

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 echo -n "Enter package name: "
 read package_name
 
-echo '[1] Clonning $package_name repository ...'
+echo "[1] Clonning $package_name repository ..."
 
 git clone https://aur.archlinux.org/$package_name.git
 
 cd ./$package_name
 
-echo '[2] Making $package_name ...'
+echo "[2] Making $package_name ..."
 makepkg -si
 
-echo '[3] Installing $package_name ...'
+echo "[3] Installing $package_name ..."
 for pkg in *.pkg.tar.zst; do sudo pacman -U $pkg; done;
 
